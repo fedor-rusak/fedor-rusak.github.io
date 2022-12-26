4820fce570c7a9ae178cfccae6fde23284c50646
Fedor Rusak <node@fedor-rusak.ru>
2022-12-27 01:45:47 +0300

    Two subprojects with welcome page

---
+++ archive/data.js
@@ -0,0 +1,425 @@
+const data = {
+    "entities": [
+        {
+            "id": 1,
+            "editable": {
+                "type": "set",
+                "name": "Seraphon team with priest!",
+                "comment": "Skink starpriest 145\nSaurus warrior alpha 135\nSkink alpha with spitter&club 105\nSkink moon boltspitter and club 75x3\nSkink with club and buckler 70x3\nsaurus warrior with spear 90x2",
+                "data": {
+                    "relations": {
+                        "images": [
+                            5,
+                            11
+                        ]
+                    }
+                }
+            },
+            "version": 4
+        },
+        {
+            "id": 2,
+            "editable": {
+                "type": "set",
+                "name": "Seraphon team with alpha warrior!",
+                "comment": "Saurus warrior alpha 135\nSaurus warrior with spear 90x3\nSaurus warrior with club 85x7",
+                "data": {}
+            },
+            "version": 1
+        },
+        {
+            "id": 3,
+            "editable": {
+                "type": "set",
+                "name": "Ironjawz team with klaw boss!",
+                "comment": "brute boss klaw 195\nbrute gore hacka 150x3\nbrute gore chopa 175\nardboy with chopp&shield 80\nardboy with choppas 80",
+                "data": {
+                    "relations": {
+                        "images": 10
+                    }
+                }
+            },
+            "version": 3
+        },
+        {
+            "id": 4,
+            "editable": {
+                "type": "set",
+                "name": "Sylvaneth team with greatsword huntmaster!",
+                "comment": "Huntmaster with greatsoword 215\nKurnoth with scythe 185\nKurnoth with great bow 195\nTree revenant scion with glave 115\nTree revenant 65\nSpite-revenant 75x3",
+                "data": {
+                    "relations": {
+                        "images": [
+                            6,
+                            7,
+                            8,
+                            9
+                        ]
+                    }
+                }
+            },
+            "version": 3
+        },
+        {
+            "id": 5,
+            "version": 1,
+            "editable": {
+                "type": "image",
+                "name": "Skink starpriest simple photo",
+                "comment": "Quick photo of skink starpriest model with white background (mostly).",
+                "data": {
+                    "src": "skink_starpriest_model.jpg"
+                }
+            }
+        },
+        {
+            "id": 6,
+            "version": 1,
+            "editable": {
+                "type": "image",
+                "name": "Kurnothi hunters box face photo",
+                "comment": "Quick photo with Kurnothi hunters with bows.",
+                "data": {
+                    "src": "kurnothi_hunters_box_face.jpg"
+                }
+            }
+        },
+        {
+            "id": 7,
+            "version": 1,
+            "editable": {
+                "type": "image",
+                "name": "Kurnothi hunters box back photo",
+                "comment": "Kurnothi with scythe and sword on the back.",
+                "data": {
+                    "src": "kurnothi_hunters_box_back.jpg"
+                }
+            }
+        },
+        {
+            "id": 8,
+            "version": 1,
+            "editable": {
+                "type": "image",
+                "name": "Tree revenants box face photo",
+                "comment": "Tree revenants box quick photo",
+                "data": {
+                    "src": "tree_revenants_box_face.jpg"
+                }
+            }
+        },
+        {
+            "id": 9,
+            "version": 1,
+            "editable": {
+                "type": "image",
+                "name": "Tree revenants box back photo",
+                "comment": "Spite-revenants version on backside of box.",
+                "data": {
+                    "src": "tree_revenants_box_back.jpg"
+                }
+            }
+        },
+        {
+            "id": 10,
+            "version": 1,
+            "editable": {
+                "type": "image",
+                "name": "Orruk brutes box face photo",
+                "comment": "These orks have some awesome weapons!",
+                "data": {
+                    "src": "orruk_brutes_box_face.jpg"
+                }
+            }
+        },
+        {
+            "id": 11,
+            "version": 1,
+            "editable": {
+                "type": "image",
+                "name": "Skinks box face photo",
+                "comment": "Swarm of small reptiles!",
+                "data": {
+                    "src": "skinks_box_face.jpg"
+                }
+            }
+        }
+    ],
+    "history": [
+        {
+            "id": 1,
+            "changedFrom": {},
+            "entityId": 1,
+            "changedTo": {
+                "type": "set",
+                "name": "Seraphon team with priest!",
+                "comment": "Skink starpriest 145\nSkink met javelin and buckler 80x6\nSkink moon boltspitter and club 75x5",
+                "data": {}
+            }
+        },
+        {
+            "id": 2,
+            "changedFrom": {},
+            "entityId": 2,
+            "changedTo": {
+                "type": "set",
+                "name": "Seraphon team with alpha warrior!",
+                "comment": "Saurus warrior alpha 135\nSaurus warrior with spear 90x3\nSaurus warrior with club 85x7",
+                "data": {}
+            }
+        },
+        {
+            "id": 3,
+            "changedFrom": {},
+            "entityId": 3,
+            "changedTo": {
+                "type": "set",
+                "name": "Ironjawz team with klaw boss!",
+                "comment": "brute boss klaw 195\nbrute gore hacka 150x2\nbrute gore chopa 175\nbrute pair choppas 140",
+                "data": {}
+            }
+        },
+        {
+            "id": 4,
+            "changedFrom": {},
+            "entityId": 4,
+            "changedTo": {
+                "type": "set",
+                "name": "Sylvaneth team with scythe huntmaster!",
+                "comment": "Huntmaster with scythe 245\nKurnoth with great sword 170\nKurnoth with great bow 195\nSpite revenant 70x5\n",
+                "data": {}
+            }
+        },
+        {
+            "id": 5,
+            "changedFrom": {
+                "type": "set",
+                "name": "Sylvaneth team with scythe huntmaster!",
+                "comment": "Huntmaster with scythe 245\nKurnoth with great sword 170\nKurnoth with great bow 195\nSpite revenant 70x5\n",
+                "data": {}
+            },
+            "entityId": 4,
+            "changedTo": {
+                "type": "set",
+                "name": "Sylvaneth team with greatsword huntmaster!",
+                "comment": "Huntmaster with greatsoword 215\nKurnoth with scythe 185\nKurnoth with great bow 195\nTree revenant scion with glave 115\nTree revenant 65\nSpite-revenant 75x3",
+                "data": {}
+            }
+        },
+        {
+            "id": 6,
+            "changedFrom": {
+                "type": "set",
+                "name": "Seraphon team with priest!",
+                "comment": "Skink starpriest 145\nSkink met javelin and buckler 80x6\nSkink moon boltspitter and club 75x5",
+                "data": {}
+            },
+            "entityId": 1,
+            "changedTo": {
+                "type": "set",
+                "name": "Seraphon team with priest!",
+                "comment": "Skink starpriest 145\nSaurus warrior alpha 135\nSkink alpha with spitter&club 105\nSkink moon boltspitter and club 75x3\nSkink with club and buckler 70x3\nsaurus warrior with spear 90x2",
+                "data": {}
+            }
+        },
+        {
+            "id": 7,
+            "changedFrom": {
+                "type": "set",
+                "name": "Ironjawz team with klaw boss!",
+                "comment": "brute boss klaw 195\nbrute gore hacka 150x2\nbrute gore chopa 175\nbrute pair choppas 140",
+                "data": {}
+            },
+            "entityId": 3,
+            "changedTo": {
+                "type": "set",
+                "name": "Ironjawz team with klaw boss!",
+                "comment": "brute boss klaw 195\nbrute gore hacka 150x3\nbrute gore chopa 175\nardboy with chopp&shield 80\nardboy with choppas 80",
+                "data": {}
+            }
+        },
+        {
+            "id": 8,
+            "changedFrom": {},
+            "entityId": 5,
+            "changedTo": {
+                "type": "image",
+                "name": "Skink starpriest simple photo",
+                "comment": "Quick photo of skink starpriest model with white background (mostly).",
+                "data": {
+                    "src": "skink_starpriest_model.jpg"
+                }
+            }
+        },
+        {
+            "id": 9,
+            "changedFrom": {
+                "type": "set",
+                "name": "Seraphon team with priest!",
+                "comment": "Skink starpriest 145\nSaurus warrior alpha 135\nSkink alpha with spitter&club 105\nSkink moon boltspitter and club 75x3\nSkink with club and buckler 70x3\nsaurus warrior with spear 90x2",
+                "data": {}
+            },
+            "entityId": 1,
+            "changedTo": {
+                "type": "set",
+                "name": "Seraphon team with priest!",
+                "comment": "Skink starpriest 145\nSaurus warrior alpha 135\nSkink alpha with spitter&club 105\nSkink moon boltspitter and club 75x3\nSkink with club and buckler 70x3\nsaurus warrior with spear 90x2",
+                "data": {
+                    "relations": {
+                        "images": 5
+                    }
+                }
+            }
+        },
+        {
+            "id": 10,
+            "changedFrom": {},
+            "entityId": 6,
+            "changedTo": {
+                "type": "image",
+                "name": "Kurnothi hunters box face photo",
+                "comment": "Quick photo with Kurnothi hunters with bows.",
+                "data": {
+                    "src": "kurnothi_hunters_box_face.jpg"
+                }
+            }
+        },
+        {
+            "id": 11,
+            "changedFrom": {},
+            "entityId": 7,
+            "changedTo": {
+                "type": "image",
+                "name": "Kurnothi hunters box back photo",
+                "comment": "Kurnothi with scythe and sword on the back.",
+                "data": {
+                    "src": "kurnothi_hunters_box_back.jpg"
+                }
+            }
+        },
+        {
+            "id": 12,
+            "changedFrom": {},
+            "entityId": 8,
+            "changedTo": {
+                "type": "image",
+                "name": "Tree revenants box face photo",
+                "comment": "Tree revenants box quick photo",
+                "data": {
+                    "src": "tree_revenants_box_face.jpg"
+                }
+            }
+        },
+        {
+            "id": 13,
+            "changedFrom": {},
+            "entityId": 9,
+            "changedTo": {
+                "type": "image",
+                "name": "Tree revenants box back photo",
+                "comment": "Spite-revenants version on backside of box.",
+                "data": {
+                    "src": "tree_revenants_box_back.jpg"
+                }
+            }
+        },
+        {
+            "id": 14,
+            "changedFrom": {
+                "type": "set",
+                "name": "Sylvaneth team with greatsword huntmaster!",
+                "comment": "Huntmaster with greatsoword 215\nKurnoth with scythe 185\nKurnoth with great bow 195\nTree revenant scion with glave 115\nTree revenant 65\nSpite-revenant 75x3",
+                "data": {}
+            },
+            "entityId": 4,
+            "changedTo": {
+                "type": "set",
+                "name": "Sylvaneth team with greatsword huntmaster!",
+                "comment": "Huntmaster with greatsoword 215\nKurnoth with scythe 185\nKurnoth with great bow 195\nTree revenant scion with glave 115\nTree revenant 65\nSpite-revenant 75x3",
+                "data": {
+                    "relations": {
+                        "images": [
+                            6,
+                            7,
+                            8,
+                            9
+                        ]
+                    }
+                }
+            }
+        },
+        {
+            "id": 15,
+            "changedFrom": {},
+            "entityId": 10,
+            "changedTo": {
+                "type": "image",
+                "name": "Orruk brutes box face photo",
+                "comment": "These orks have some awesome weapons!",
+                "data": {
+                    "src": "orruk_brutes_box_face.jpg"
+                }
+            }
+        },
+        {
+            "id": 16,
+            "changedFrom": {
+                "type": "set",
+                "name": "Ironjawz team with klaw boss!",
+                "comment": "brute boss klaw 195\nbrute gore hacka 150x3\nbrute gore chopa 175\nardboy with chopp&shield 80\nardboy with choppas 80",
+                "data": {}
+            },
+            "entityId": 3,
+            "changedTo": {
+                "type": "set",
+                "name": "Ironjawz team with klaw boss!",
+                "comment": "brute boss klaw 195\nbrute gore hacka 150x3\nbrute gore chopa 175\nardboy with chopp&shield 80\nardboy with choppas 80",
+                "data": {
+                    "relations": {
+                        "images": 10
+                    }
+                }
+            }
+        },
+        {
+            "id": 17,
+            "changedFrom": {},
+            "entityId": 11,
+            "changedTo": {
+                "type": "image",
+                "name": "Skinks box face photo",
+                "comment": "Swarm of small reptiles!",
+                "data": {
+                    "src": "skinks_box_face.jpg"
+                }
+            }
+        },
+        {
+            "id": 18,
+            "changedFrom": {
+                "type": "set",
+                "name": "Seraphon team with priest!",
+                "comment": "Skink starpriest 145\nSaurus warrior alpha 135\nSkink alpha with spitter&club 105\nSkink moon boltspitter and club 75x3\nSkink with club and buckler 70x3\nsaurus warrior with spear 90x2",
+                "data": {
+                    "relations": {
+                        "images": 5
+                    }
+                }
+            },
+            "entityId": 1,
+            "changedTo": {
+                "type": "set",
+                "name": "Seraphon team with priest!",
+                "comment": "Skink starpriest 145\nSaurus warrior alpha 135\nSkink alpha with spitter&club 105\nSkink moon boltspitter and club 75x3\nSkink with club and buckler 70x3\nsaurus warrior with spear 90x2",
+                "data": {
+                    "relations": {
+                        "images": [
+                            5,
+                            11
+                        ]
+                    }
+                }
+            }
+        }
+    ]
+};
